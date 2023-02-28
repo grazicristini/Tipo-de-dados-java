2993f99ab3e0cf4aea439272063087245f0c91e7
grazicristini <grazielicristhyny2002@gmail.com>
2023-02-28 14:10:00 +0000

    salvar

---
+++ exercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int n1;
+
+        System.out.println("Informe o primeiro número: "); 
+        n1 = input.nextInt();
+
+        if (n1 > 0)
+        {
+            System.out.println("Número positivo");
+        }else if (n1 < 0){
+            System.out.println("Número negativo");
+        }else {
+            System.out.println("Número É 0");
+        }
+    }
+}
