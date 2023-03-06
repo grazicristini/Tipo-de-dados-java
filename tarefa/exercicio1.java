a712c4c5db0b1e7081a888b377af8c27778e9e2b
grazicristini <grazielicristhyny2002@gmail.com>
2023-03-06 10:43:57 +0000

    salvar

--- tarefa/exercicio1.java
+++ tarefa/exercicio1.java
@@ -7,20 +7,21 @@ public class exercicio1 {
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        double salarioAtual, salarioNovo;
+        double SalarioAtual, SalarioNovo;
 
         System.out.println("Informe seu Salário Atual: "); 
-        salarioAtual = input.nextFloat();
+        SalarioAtual = input.nextDouble();
 
-        if (salarioAtual > 1000 && salarioAtual < 2000) {
+        if (SalarioAtual > 1000 && SalarioAtual < 2000) {
+            SalarioNovo = SalarioAtual * 1.15 ;
             System.out.println("Seu novo salário é:  \n");
-            salarioNovo = salarioAtual * 1.15 ;
+            
 
-        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
-            salarioNovo = salarioAtual * 1.10;
+        }else if (SalarioAtual > 2000 && SalarioAtual < 3000) {
+            SalarioNovo = SalarioAtual * 1.10;
 
-        }else if (salarioAtual > 2000 && salarioAtual < 3000) {
-            salarioNovo = salarioAtual * 1.05;
+        }else if (SalarioAtual > 2000 && SalarioAtual < 3000) {
+            SalarioNovo = SalarioAtual * 1.05;
         }
     }
 }
